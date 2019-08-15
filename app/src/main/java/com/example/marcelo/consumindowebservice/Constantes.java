55c86307d01e5c2e58be98d9c8a59adb49a4d1fc
hitalo.pcarvalho <carvhitalo82@gmail.com>
2019-08-15 12:04:09 -0300

    Initial commit

---
+++ app/src/main/java/com/example/marcelo/consumindowebservice/Constantes.java
@@ -0,0 +1,13 @@
+package com.example.marcelo.consumindowebservice;
+
+/**
+ * Created by MARCELO on 11/12/2017.
+ */
+
+public class Constantes {
+
+    /* EndPoints */
+    /* Altere aqui para o endereço do servidor onde esta publicado o webservice */
+    public final static String urlLogin = "http://192.168.15.12/WebServiceAndroid/login.php";
+    public final static String urlRegistrar = "http://192.168.15.12/WebServiceAndroid/registrar.php";
+}
